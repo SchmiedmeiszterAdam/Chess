ca929d5e9aec35c91a17a713dd5a3324525e7201
SchmiedmeiszterAdam <smikuadam@gmail.com>
2022-01-25 18:22:02 +0100

    pawn ell nem jó

--- ChessPiece/Pawn.js
+++ ChessPiece/Pawn.js
@@ -1,25 +1,33 @@
-class Pawn extends ChessPieces{
-    constructor(element,color,tableParent,position1,position2){
-        super(element,color,tableParent,position1,position2)
+class Pawn extends ChessPieces {
+    constructor(element, color, tableParent, position1, position2) {
+        super(element, color, tableParent, position1, position2)
         this.firstMove = true
     }
-    highlight(){
-        if(this.color === "black"){
+    highlight() {
+        if (this.color === "black") {
             this.highlightHelp(1)
         }
-        else{
+        else {
             this.highlightHelp(-1)
         }
     }
-    highlightHelp(c){
-        let a = 1*c
-        let b = 2*c
-        this.tableParent.table[this.positions[0]+a][this.positions[1]].append("<div class = 'highlight'></div>")
-        if(this.firstMove){
-            this.tableParent.table[this.positions[0]+b][this.positions[1]].append("<div class = 'highlight'></div>")
+    highlightHelp(c) {
+        console.log(this.positions)
+        if (this.tableParent.table[this.positions[0] + (1 * c)][this.positions[1]].is(':empty')) {
+            this.tableParent.table[this.positions[0] + (1 * c)][this.positions[1]].append("<div class = 'highlight'></div>")
+        }
+        console.log(this.positions[0] + (1 * c),this.positions[1] + c)
+        if(this.positions[1] !=0 && !this.tableParent.table[this.positions[0] + (1 * c)][this.positions[1] + c].is(':empty')){
+            this.tableParent.table[this.positions[0] + (1 * c)][this.positions[1] + c].append("<div class = 'red-highlight'></div>")
+        }
+        if(this.positions[1] !=7 && !this.tableParent.table[this.positions[0] + (1 * c)][this.positions[1] + c].is(':empty')){
+            this.tableParent.table[this.positions[0] + (1 * c)][this.positions[1] +c].append("<div class = 'red-highlight'></div>")
+        }
+        if (this.firstMove) {
+            this.tableParent.table[this.positions[0] + (2 * c)][this.positions[1]].append("<div class = 'highlight'></div>")
         }
     }
-    move(){
+    move() {
 
     }
 }
