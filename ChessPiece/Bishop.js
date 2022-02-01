e43e26820ed0e353e664f101759032a99b2cc135
SchmiedmeiszterAdam <smikuadam@gmail.com>
2022-02-01 20:19:07 +0100

    bishop move + hit

--- ChessPiece/Bishop.js
+++ ChessPiece/Bishop.js
@@ -1,7 +1,6 @@
 class Bishop extends ChessPiece {
     constructor(element, color, tableParent, position1, position2) {
         super(element, color, tableParent, position1, position2)
-
     }
     highlight() {
         let i = this.positions[0]
